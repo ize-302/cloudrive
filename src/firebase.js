1535d1e30f17313b3622f9642efe6052eecaad20
adavize <adavize@thepoint.africa>
2020-08-20 21:53:13 +0100

    ft: add firebase storage config

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 
 // configuration
 const firebaseConfig = {
@@ -19,3 +20,4 @@ firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const db = firebase.firestore();
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
+export const storage = firebase.storage();
