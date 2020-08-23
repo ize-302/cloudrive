8502bc5cb35645f3062237c284ce8252f06d2033
adavize <adavize@thepoint.africa>
2020-08-23 22:50:00 +0100

    Clean up :sparkles:

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,5 @@
-import React, { useContext, useState, useEffect } from "react";
-import { Text, Box, SimpleGrid } from "@chakra-ui/core";
+import React, { useState, useEffect } from "react";
 import { ShowFolders } from "../components/Folders";
-import { FileCard } from "../components/FileCard";
-import { AuthContext } from "../contexts/AuthContext";
 import { db } from "../firebase";
 
 const HomePage = () => {
@@ -21,31 +18,9 @@ const HomePage = () => {
   }, [files]);
 
   return (
-    <AuthContext.Consumer>
-      {(context) => (
-        <>
-          <ShowFolders />
-          {/* <Box mt={10}>
-            <Text fontSize={"2xl"} mb={5} fontWeight="bold">
-              Recent uploads
-            </Text>
-          </Box>
-          <SimpleGrid columns={[1, 2, 3, 4, 5]} spacing={[3, 5, 10]}>
-            {files.map((item) => {
-              return (
-                <FileCard
-                  key={item.id}
-                  file_name={item.file_name}
-                  id={item.id}
-                  date_added={item.date_added}
-                  folder_name={item.folder_name}
-                />
-              );
-            })}
-          </SimpleGrid> */}
-        </>
-      )}
-    </AuthContext.Consumer>
+    <>
+      <ShowFolders />
+    </>
   );
 };
 
